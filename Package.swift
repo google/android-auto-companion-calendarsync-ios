0b25977eaef8d0a3fb7a9f3d7c0acb85a30b50ef
Tom Pelaia <tompelaia@google.com>
2023-02-23 14:26:32 -0500

    Depend on the Companion preview branch.

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
       targets: ["AndroidAutoCalendarSync"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/google/android-auto-companion-ios.git", from: "3.0.0"),
+    .package(url: "https://github.com/google/android-auto-companion-ios.git", branch: "preview"),
     .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.20.0"),
   ],
   targets: [
